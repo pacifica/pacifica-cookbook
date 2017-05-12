58034cb7f3b833cd3648a3079736e3b6020bf43c
dmlb2000 <dmlb2000@gmail.com>
2017-05-11 19:00:39 -0700

    version bump elasticsearch

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -1,11 +1,6 @@
 default['postgresql']['password']['postgres'] = 'postgres'
 default['java']['jdk_version'] = '8'
-case node['platform_family']
-when 'rhel'
-  override['elasticsearch']['install']['version'] = '5.2.1'
-when 'debian'
-  default['elasticsearch']['install']['version'] = '5.2.0'
-end
+default['elasticsearch']['install']['version'] = '5.4.0'
 
 default['rabbitmq']['loopback_users'] = []
 default['rabbitmq']['virtualhosts'] = %w(/cart /ingest /uploader)
