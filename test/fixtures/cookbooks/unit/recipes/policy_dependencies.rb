d84b8b2e5f5a2c1f9ce33d52ad6c764629619866
David Brown <dmlb2000@gmail.com>
2017-12-10 22:09:59 -0800

    closer to working

--- test/fixtures/cookbooks/unit/recipes/policy_dependencies.rb
+++ test/fixtures/cookbooks/unit/recipes/policy_dependencies.rb
@@ -7,6 +7,7 @@ end
 execute 'sleep 15'
 
 execute 'Load Metadata Set' do
+  environment LD_LIBRARY_PATH: '/opt/rh/python27/root/usr/lib64', LD_RUN_PATH: '/opt/rh/python27/root/usr/lib64'
   command '/opt/default/bin/python test_files/loadit.py'
   cwd "#{Chef::Config[:file_cache_path]}/pacifica-metadata"
 end
