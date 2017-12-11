cb0d464ac3a10ff8f58d83a0727e8a6e102b9c23
David Brown <dmlb2000@gmail.com>
2017-12-11 12:06:59 -0800

    this adds uniqueid service to kitchen

--- test/fixtures/cookbooks/unit/recipes/uniqueid_dependencies.rb
+++ test/fixtures/cookbooks/unit/recipes/uniqueid_dependencies.rb
@@ -1,7 +1,3 @@
-include_recipe 'unit::archiveinterface'
-execute 'Load Archive Data' do
-  command 'curl -X PUT http://127.0.0.1:8080/foo.txt -d1234'
-end
 include_recipe 'chef-sugar'
 include_recipe 'build-essential'
 packages = if rhel?
@@ -34,14 +30,11 @@ service 'MySQL Service' do
   service_name service_name
   action [:start, :enable]
 end
-execute 'Create Cart Database' do
-  command "#{mysql_bin} -e 'create database pacifica_cart;'"
-  not_if "#{mysql_bin} -e 'show databases;' | grep -q pacifica_cart"
+execute 'Create UniqueID Database' do
+  command "#{mysql_bin} -e 'create database pacifica_uniqueid;'"
+  not_if "#{mysql_bin} -e 'show databases;' | grep -q pacifica_uniqueid"
 end
-execute 'Grant/Create Cart User' do
-  command "#{mysql_bin} -e 'grant all on pacifica_cart.* to cartd@'\\''localhost'\\'' identified by '\\''cartd'\\'';'"
-  not_if "#{mysql_bin} -e 'select User from mysql.user;' | grep cartd"
+execute 'Grant/Create UniqueID User' do
+  command "#{mysql_bin} -e 'grant all on pacifica_uniqueid.* to uniqueid@'\\''localhost'\\'' identified by '\\''uniqueid'\\'';'"
+  not_if "#{mysql_bin} -e 'select User from mysql.user;' | grep uniqueid"
 end
-include_recipe 'rabbitmq'
-include_recipe 'rabbitmq::virtualhost_management'
-include_recipe 'rabbitmq::user_management'
