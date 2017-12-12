7cfb96d91ed9a73b3ddd9f2b855f3a3583102d96
David Brown <dmlb2000@gmail.com>
2017-12-12 15:16:27 -0800

    try getting an OSI approved license string

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'pacifica'
 maintainer 'David Brown'
 maintainer_email 'dmlb2000@gmail.com'
-license 'Apache v2.0'
+license 'Apache-2.0'
 description 'Installs/Configures pacifica'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 if respond_to?(:issues_url)
