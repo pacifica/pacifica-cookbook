9c9fca9be058d077abf52d3063e70f642655a172
David Brown <dmlb2000@gmail.com>
2017-12-12 21:02:15 -0800

    add more spec

--- spec/unit/recipes/metadata_spec.rb
+++ spec/unit/recipes/metadata_spec.rb
@@ -6,16 +6,11 @@
 
 require 'spec_helper'
 
-describe 'unit::ingest' do
+describe 'unit::metadata' do
   before do
     stub_command("psql -c '\\l' | grep -q pacifica_metadata").and_return(true)
     stub_command("psql -c 'SELECT rolname FROM pg_roles;' | grep -q pacifica").and_return(true)
     stub_command("psql -c '\\l' | grep -q pacifica=").and_return(true)
-    stub_command("curl localhost:8121/users | grep -q dmlb2001").and_return(true)
-    stub_command("/usr/bin/mysql -e 'show databases;' | grep -q pacifica_uniqueid").and_return(true)
-    stub_command("/usr/bin/mysql -e 'select User from mysql.user;' | grep uniqueid").and_return(true)
-    stub_command("/usr/bin/mysql -e 'show databases;' | grep -q pacifica_ingest").and_return(true)
-    stub_command("/usr/bin/mysql -e 'select User from mysql.user;' | grep ingest").and_return(true)
   end
   supported_platforms.each do |platform, versions|
     versions.each do |version|
@@ -26,12 +21,8 @@ describe 'unit::ingest' do
           ).converge(described_recipe)
         end
 
-        it 'Setups pacifica ingest frontend' do
-          expect(chef_run).to create_pacifica_ingestfrontend('default')
-        end
-
-        it 'Setups pacifica ingest backend' do
-          expect(chef_run).to create_pacifica_ingestbackend('default')
+        it 'Setups pacifica metadata' do
+          expect(chef_run).to create_pacifica_metadata('default')
         end
 
         it 'Converges successfully for default' do
