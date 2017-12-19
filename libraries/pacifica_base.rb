bae7c816585878aa90637e80e79383f4b7dff97c
David Brown <dmlb2000@gmail.com>
2017-12-18 16:06:54 -0800

    I like dashed resource name instead of underscore

--- libraries/pacifica_base.rb
+++ libraries/pacifica_base.rb
@@ -6,7 +6,7 @@ module PacificaCookbook
   class PacificaBase < ChefCompat::Resource
     include PacificaHelpers::BaseDirectories
     property :name, String, name_property: true
-    property :service_name, String, default: lazy { "#{name}-#{self.class.name.gsub(/^.*::/, '')}" }
+    property :service_name, String, default: lazy { "#{name}-#{resource_name.gsub(/_/, '-')}" }
     property :script_name, String, default: lazy { service_name }
     property :config_name, String, default: lazy { "#{service_name}.ini" }
     property :command_name, String, default: 'Server.py'
