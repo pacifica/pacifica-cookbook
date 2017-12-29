9ad1562db6d7f410a889a7d97e5135c7749bafa5
David Brown <dmlb2000@gmail.com>
2017-12-29 11:09:20 -0800

    better platform versions

--- spec/support/supported_platforms.rb
+++ spec/support/supported_platforms.rb
@@ -1,7 +1,7 @@
 def supported_platforms
   _platforms = {
-    'centos' => ['7.2.1511', '6.9'],
-    'redhat' => ['7.1'],
+    'centos' => ['7.4.1708', '6.9'],
+    'redhat' => ['7.4'],
     'ubuntu' => ['16.04'],
   }
 end
