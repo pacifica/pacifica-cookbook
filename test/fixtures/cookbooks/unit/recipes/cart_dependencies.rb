937918d7fa81fa4f8765e50a7b92459c8d2bb5a0
David Brown <dmlb2000@gmail.com>
2018-06-15 14:01:26 -0700

    should fix upload to archive interface

--- test/fixtures/cookbooks/unit/recipes/cart_dependencies.rb
+++ test/fixtures/cookbooks/unit/recipes/cart_dependencies.rb
@@ -1,7 +1,10 @@
 include_recipe 'unit::archiveinterface'
 include_recipe 'pacifica-dependencies::mysql_service'
 include_recipe 'pacifica-dependencies::rabbitmq'
-execute 'Load Archive Data' do
-  command 'curl -X PUT http://127.0.0.1:8080/foo.txt -d1234'
+package 'file'
+bash 'Load Archive Data' do
+  code <<EOH
+  echo -n 1234 | curl -X PUT -T - -H 'Content-Length: 4' http://127.0.0.1:8080/foo.txt
+EOH
 end
 include_recipe 'pacifica-dependencies::cartdb_setup'
