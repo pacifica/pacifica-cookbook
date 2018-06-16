022f60e104a2d6eb3247ecbca9ca843897e1f33c
David Brown <dmlb2000@gmail.com>
2018-06-15 19:09:27 -0700

    we need a basic config for uploader --help to work

--- libraries/pacifica_uploadercli.rb
+++ libraries/pacifica_uploadercli.rb
@@ -9,6 +9,98 @@ module PacificaCookbook
     property :pip_install_opts, Hash, default: {
       command: '-m pip install git+https://github.com/pacifica/pacifica-cli-uploader.git@master',
     }
+    property :config_opts, Hash, default: {
+      variables: {
+        content: <<EOH
+[
+  {
+    "destinationTable": "Transactions.submitter",
+    "displayFormat": "{_id} - {first_name} {last_name}",
+    "displayTitle": "Currently Logged On",
+    "displayType": "logged_on",
+    "metaID": "logon",
+    "queryDependency": {},
+    "queryFields": [
+      "first_name",
+      "last_name",
+      "_id"
+    ],
+    "sourceTable": "users",
+    "value": "",
+    "valueField": "_id"
+  },
+  {
+    "destinationTable": "Transactions.proposal",
+    "displayFormat": "{_id} - {title}",
+    "displayTitle": "Proposal ID",
+    "displayType": "select",
+    "metaID": "proposal",
+    "queryDependency": {
+      "instrument_id": "instrument"
+    },
+    "queryFields": [
+      "_id",
+      "title"
+    ],
+    "sourceTable": "proposals",
+    "value": "",
+    "valueField": "_id"
+  },
+  {
+    "destinationTable": "Transactions.instrument",
+    "displayFormat": "{_id} {name_short} - {display_name}",
+    "displayTitle": "Instrument ID",
+    "displayType": "select",
+    "metaID": "instrument",
+    "queryDependency": {},
+    "queryFields": [
+      "_id",
+      "name_short",
+      "display_name"
+    ],
+    "sourceTable": "instruments",
+    "value": "",
+    "valueField": "_id"
+  },
+  {
+    "destinationTable": "TransactionKeyValue",
+    "displayFormat": "{_id} - {first_name} {last_name}",
+    "displayTitle": "User ID the data is for",
+    "displayType": "select",
+    "key": "User of Record",
+    "metaID": "user-of-record",
+    "queryDependency": {
+      "proposal_id": "proposal"
+    },
+    "queryFields": [
+      "first_name",
+      "last_name",
+      "_id"
+    ],
+    "sourceTable": "users",
+    "value": "",
+    "valueField": "_id"
+  },
+  {
+    "destinationTable": "NothingReally",
+    "directoryOrder": 0,
+    "displayFormat": "Proposal ID {_id}",
+    "displayType": "directoryTree",
+    "metaID": "directory-proposal",
+    "queryDependency": {
+      "_id": "proposal"
+    },
+    "queryFields": [
+      "_id"
+    ],
+    "sourceTable": "proposals",
+    "value": "",
+    "valueField": "_id"
+  }
+]
+EOH
+      }
+    }
     property :service_disabled, [TrueClass, FalseClass], default: true
   end
 end
--- test/integration/uploadercli/uploadercli_test.rb
+++ test/integration/uploadercli/uploadercli_test.rb
@@ -5,6 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-describe command('/opt/default/bin/CLIUploader --help') do
+describe command('/opt/default/bin/CLIUploader --config /opt/default/default-pacifica-uploadercli.ini --help') do
   its(:exit_status) { should eq 0 }
 end
