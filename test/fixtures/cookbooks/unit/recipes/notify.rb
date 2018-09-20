5d00c4accec54fd3999d813c9bce9f082d1038e0
David Brown <dmlb2000@gmail.com>
2018-09-20 16:35:55 -0700

    Add notifications frontend and backend (#21)
    This adds notifications frontend and backend. This also adds the
    chef 14 update.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

---
+++ test/fixtures/cookbooks/unit/recipes/notify.rb
@@ -0,0 +1,3 @@
+include_recipe 'unit::notify_dependencies'
+pacifica_notifyfrontend 'default'
+pacifica_notifybackend 'default'
--- travis/pre-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-source ~/virtualenv/python2.7/bin/activate
-python --version
-pip install --upgrade pip setuptools
-pip install pre-commit
-pre-commit run -a
